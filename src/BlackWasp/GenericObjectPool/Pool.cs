99295bc39675e87b10fe53e8aaf345df6526e3f7
Tim <tim@timcollins.ie>
2015-09-15 21:47:24 +0100

    Initial check-in of Generic Object Pool design pattern.

---
+++ src/BlackWasp/GenericObjectPool/Pool.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace BlackWasp.GenericObjectPool
+{
+    /// <summary>
+    /// See http://www.blackwasp.co.uk/GenericObjectPool.aspx
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Pool<T> where T : new()
+    {
+        private List<T> _available = new List<T>(); 
+        private List<T> _inUse = new List<T>(); 
+
+
+    }
+}
