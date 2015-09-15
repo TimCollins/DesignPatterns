44709ebf029a671e64af7e198e317a5a744050a5
Tim <tim@timcollins.ie>
2015-09-15 21:43:41 +0100

    Added Abstract Factory example from BlackWasp.co.uk.

---
+++ src/BlackWasp/AbstractFactory/Example.cs
@@ -0,0 +1,64 @@
+﻿namespace BlackWasp.AbstractFactory
+{
+    public class ExampleClient
+    {
+        private readonly Packaging _packaging;
+        private readonly DeliveryDocument _deliveryDocument;
+
+        public ExampleClient(PurchaseFactory factory)
+        {
+            _packaging = factory.CreatePackaging();
+            _deliveryDocument = factory.CreateDeliveryDocument();
+        }
+
+        public Packaging ClientPackaging
+        {
+            get { return _packaging; }
+        }
+
+        public DeliveryDocument ClientDocument
+        {
+            get { return _deliveryDocument; }
+        }
+    }
+
+    public abstract class PurchaseFactory
+    {
+        internal abstract Packaging CreatePackaging();
+        internal abstract DeliveryDocument CreateDeliveryDocument();
+    }
+
+    public class StandardPurchaseFactory : PurchaseFactory
+    {
+        internal override Packaging CreatePackaging()
+        {
+            return new StandardPackaging();
+        }
+
+        internal override DeliveryDocument CreateDeliveryDocument()
+        {
+            return new PostalLabel();
+        }
+    }
+
+    public class DelicatePurchaseFactory : PurchaseFactory
+    {
+        internal override Packaging CreatePackaging()
+        {
+            return new ShockProofPackaging();
+        }
+
+        internal override DeliveryDocument CreateDeliveryDocument()
+        {
+            return new CourierManifest();
+        }
+    }
+
+    public abstract class Packaging { }
+    internal class StandardPackaging : Packaging { }
+    internal class ShockProofPackaging : Packaging { }
+
+    public abstract class DeliveryDocument { }
+    internal class PostalLabel : DeliveryDocument { }
+    internal class CourierManifest : DeliveryDocument { }
+}
--- src/PatternRunner/Program.cs
+++ src/PatternRunner/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace PatternRunner
+﻿using System;
+using BlackWasp.AbstractFactory;
+
+namespace PatternRunner
 {
     class Program
     {
         static void Main(string[] args)
         {
+            PurchaseFactory spf = new StandardPurchaseFactory();
+            ExampleClient standardClient = new ExampleClient(spf);
+
+            Console.WriteLine(standardClient.ClientPackaging.GetType());
+            Console.WriteLine(standardClient.ClientDocument.GetType());
+
+            PurchaseFactory dpf = new DelicatePurchaseFactory();
+            ExampleClient delicateClient = new ExampleClient(dpf);
+
+            Console.WriteLine(delicateClient.ClientPackaging.GetType());
+            Console.WriteLine(delicateClient.ClientDocument.GetType());
+
+            ConsoleUtils.WaitForEscape();
         }
     }
 }
