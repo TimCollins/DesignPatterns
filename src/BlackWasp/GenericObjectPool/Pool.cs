ba47ceb6fd36cf654140a08c6524e034f47436b1
Tim <tim@timcollins.ie>
2015-09-16 20:16:55 +0100

    Completed implementation of Pool.cs. Added PooledObject.cs.

--- src/BlackWasp/GenericObjectPool/Pool.cs
+++ src/BlackWasp/GenericObjectPool/Pool.cs
@@ -4,13 +4,50 @@ namespace BlackWasp.GenericObjectPool
 {
     /// <summary>
     /// See http://www.blackwasp.co.uk/GenericObjectPool.aspx
+    /// This is a generic class that represents a pool of objects.
+    /// A default constructor is added 
     /// </summary>
     /// <typeparam name="T"></typeparam>
     public class Pool<T> where T : new()
     {
-        private List<T> _available = new List<T>(); 
-        private List<T> _inUse = new List<T>(); 
+        private readonly List<T> _available = new List<T>(); 
+        private readonly List<T> _inUse = new List<T>();
 
+        public T Get()
+        {
+            lock (_available)
+            {
+                if (_available.Count != 0)
+                {
+                    T obj = _available[0];
+                    _inUse.Add(obj);
+                    _available.RemoveAt(0);
 
+                    return obj;
+                }
+                else
+                {
+                    T obj = new T();
+                    _inUse.Add(obj);
+
+                    return obj;
+                }
+            }
+        }
+
+        public void Release(T obj)
+        {
+            CleanUp(obj);
+            
+            lock (_available)
+            {
+                _available.Add(obj);
+                _inUse.Remove(obj);
+            }
+        }
+
+        private void CleanUp(T obj)
+        {
+        }
     }
 }
---
+++ src/BlackWasp/GenericObjectPool/PooledObject.cs
@@ -0,0 +1,19 @@
+﻿namespace BlackWasp.GenericObjectPool
+{
+    public class PooledObject
+    {
+        private static readonly object _idLock = new object();
+        private static int _nextId = 1;
+
+        public int PermanentId { get; private set; }
+        public string Name { get; set; }
+
+        public PooledObject()
+        {
+            lock (_idLock)
+            {
+                PermanentId = _nextId++;
+            }
+        }
+    }
+}
