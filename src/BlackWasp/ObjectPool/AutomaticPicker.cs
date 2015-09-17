be30f68350f9378540e6355780c2d1545c2f773d
Tim <tim@timcollins.ie>
2015-09-17 20:56:17 +0100

    Added PickerPool.cs. Completed implementation of AutomaticPicker.cs.

--- src/BlackWasp/ObjectPool/AutomaticPicker.cs
+++ src/BlackWasp/ObjectPool/AutomaticPicker.cs
@@ -1,6 +1,45 @@
-﻿namespace BlackWasp.ObjectPool
+﻿using System;
+
+namespace BlackWasp.ObjectPool
 {
-    public class AutomaticPicker
+    public class AutomatedPicker
     {
+        public int PickerId { get; set; }
+        public string CurrentLocation { get; set; }
+        public string Carrying { get; set; }
+
+        public AutomatedPicker(int pickerId)
+        {
+            PickerId = pickerId;
+            CurrentLocation = "Recharging Station";
+        }
+
+        public void Identify(string code)
+        {
+            ShowMessage("Identified as " + code);
+        }
+
+        public void GoToLocation(string location)
+        {
+            CurrentLocation = location;
+            ShowMessage("At " + location);
+        }
+
+        public void Pick(string item)
+        {
+            Carrying = item;
+            ShowMessage("Picked Up " + item);
+        }
+
+        public void Drop()
+        {
+            ShowMessage("Dropped" + Carrying);
+            Carrying = null;
+        }
+
+        public void ShowMessage(string msg)
+        {
+            Console.WriteLine("Picker {0} : {1}", PickerId, msg);
+        }
     }
 }
---
+++ src/BlackWasp/ObjectPool/PickerPool.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlackWasp.ObjectPool
+{
+    public static class PickerPool
+    {
+        private const int PickerCount = 5;
+
+        private static List<AutomatedPicker> _available = new List<AutomatedPicker>();
+        private static List<AutomatedPicker> _inUse = new List<AutomatedPicker>();
+
+        static PickerPool()
+        {
+            for (int i = 0; i <= PickerCount; i++)
+            {
+                _available.Add(new AutomatedPicker(i));
+            }
+        }
+
+        public static AutomatedPicker GetPicker()
+        {
+            lock (_available)
+            {
+                if (_available.Count == 0)
+                {
+                    throw new InvalidOperationException("No pickers are available");
+                }
+
+                AutomatedPicker picker = _available[0];
+                _inUse.Add(picker);
+                _available.RemoveAt(0);
+
+                return picker;
+            }
+        }
+
+        public static void ReleasePicker(AutomatedPicker picker)
+        {
+            Reset(picker);
+
+            lock (_available)
+            {
+                _available.Add(picker);
+                _inUse.Remove(picker);
+            }
+        }
+
+        private static void Reset(AutomatedPicker picker)
+        {
+            if (picker.Carrying != null)
+            {
+                picker.Drop();
+            }
+
+            picker.GoToLocation("Recharging Station");
+        }
+    }
+}
