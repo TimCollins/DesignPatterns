9a9ea013d1b12a7e6c1e95a93fb0eef29f6ed23c
Tim <tim@timcollins.ie>
2015-09-21 20:24:00 +0100

    Added basic example of Factory pattern.

---
+++ src/BlackWasp/Factory/Basic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BlackWasp.Factory
+{
+    public abstract class ProductBase
+    {
+        
+    }
+
+    public class ConcreteProduct1 : ProductBase
+    {
+    }
+
+    public class ConcreteProduct2 : ProductBase
+    {
+    }
+
+    public abstract class FactoryBase
+    {
+        public abstract ProductBase FactoryMethod(int type);
+    }
+
+    public class ConcreteFactory : FactoryBase
+    {
+        public override ProductBase FactoryMethod(int type)
+        {
+            switch (type)
+            {
+                case 1:
+                    return new ConcreteProduct1();
+                case 2:
+                    return new ConcreteProduct2();
+                default:
+                    throw new ArgumentException("Invalid type", "type");
+            }
+        }
+    }
+}
