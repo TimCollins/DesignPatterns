3067f90dfe46b67741dda3f11fbb4e319f3b814a
Tim <tim@timcollins.ie>
2015-09-21 20:36:34 +0100

    Added some comments.

--- src/BlackWasp/Factory/Basic.cs
+++ src/BlackWasp/Factory/Basic.cs
@@ -2,24 +2,40 @@
 
 namespace BlackWasp.Factory
 {
+    /// <summary>
+    /// This is the base class for the different object types that the factory can create.
+    /// </summary>
     public abstract class ProductBase
     {
         
     }
 
+    /// <summary>
+    /// A subclass of ProductBase which would contain specific functionality in a real example.
+    /// </summary>
     public class ConcreteProduct1 : ProductBase
     {
     }
 
+    /// <summary>
+    /// A subclass of ProductBase which would contain specific functionality in a real example.
+    /// </summary>
     public class ConcreteProduct2 : ProductBase
     {
     }
 
+    /// <summary>
+    /// The base class for the actual factory classes that will create new objects.
+    /// </summary>
     public abstract class FactoryBase
     {
         public abstract ProductBase FactoryMethod(int type);
     }
 
+    /// <summary>
+    /// This inherits the functionality from FactoryBase and overrides the factory method as
+    /// required to handle the creation of specific objects or handle an error.
+    /// </summary>
     public class ConcreteFactory : FactoryBase
     {
         public override ProductBase FactoryMethod(int type)
