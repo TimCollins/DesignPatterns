13e102bc70b5172ab36a20ff147f4d90af9e9c72
Tim Collins <tim.collins@hpe.com>
2016-10-17 18:09:23 +0100

    Added Singleton example.

---
+++ src/BlackWasp/Singleton/ApplicationState.cs
@@ -0,0 +1,24 @@
+﻿namespace BlackWasp.Singleton
+{
+    public sealed class ApplicationState
+    {
+        // Singleton implementation
+        private static ApplicationState _instance;
+        private static object _lock = new object();
+
+        private ApplicationState() { }
+
+        public ApplicationState GetApplicationState()
+        {
+            lock (_lock)
+            {
+                return _instance ?? (_instance = new ApplicationState());
+            }
+        }
+
+        // Application state information
+        public string UserName { get; set; }
+        public int UserId { get; set; }
+        public bool IsConnected { get; set; }
+    }
+}
---
+++ src/BlackWasp/Singleton/Singleton.cs
@@ -0,0 +1,24 @@
+﻿namespace BlackWasp.Singleton
+{
+    public sealed class Singleton
+    {
+        private static Singleton _instance;
+        private static readonly object _lock = new object();
+
+        private Singleton() { }
+
+        public static Singleton GetSingleton()
+        {
+            lock (_lock)
+            {
+                // Could also do return _instance ?? (_instance = new Singleton());
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
